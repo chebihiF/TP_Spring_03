a5370033d3d7b261b69fa31935adf2977ecd684b
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-23 13:45:43 +0100

    add context

--- src/main/java/metier/CustomerMetier.java
+++ src/main/java/metier/CustomerMetier.java
@@ -2,13 +2,17 @@ package metier;
 
 import dao.ICustomerDao;
 import entities.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CustomerMetier implements ICustomerMetier{
-
     // IOC
     private ICustomerDao dao ;
+
+    public void setDao(ICustomerDao dao) {
+        this.dao = dao;
+    }
 
     public Customer subscribeCustomer(Customer customer) throws Exception {
         if(!customer.getEmail().contains("gmail")) throw new Exception("email invalid");
