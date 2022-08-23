24165676ed5f32d5c279f6313993497417c6ee6b
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-23 13:14:59 +0100

    implement addCustomer & update custommer

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -1,17 +1,46 @@
 package dao;
 
 import entities.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import util.HibernateUtil;
 
 import java.util.List;
 
 public class CustomerDao implements ICustomerDao{
 
+    private SessionFactory factory = HibernateUtil.getFactory();
+    private Session session = null ;
+
     public Customer addCustomer(Customer customer) throws Exception {
-        return null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            Long id = (Long) session.save(customer);
+            customer.setId(id);
+            session.getTransaction().commit();
+            return customer;
+        } catch (Exception ex){
+            session.getTransaction().rollback();
+            throw new Exception(ex.getMessage());
+        }finally{
+            session.close();
+        }
     }
 
     public Customer updateCustomer(Customer customer) throws Exception {
-        return null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            session.update(customer);
+            session.getTransaction().commit();
+            return customer;
+        } catch (Exception ex){
+            session.getTransaction().rollback();
+            throw new Exception(ex.getMessage());
+        }finally{
+            session.close();
+        }
     }
 
     public Customer deleteCustomer(Long id) throws Exception {
